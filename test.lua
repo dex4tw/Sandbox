cd6c0423b35175f44c15f5ff86b43164786a67ba
dex <121840104+dex4tw@users.noreply.github.com>
2024-05-11 16:48:59 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,15 @@
 -- assuming the game has the 'owner' keyword
-print(owner.name)
-print('woo')
+local localPlayer = owner
+local Character = localPlayer.Character
+
+local Tool = Instance.new("Tool", localPlayer.Backpack)
+Tool.RequiresHandle = false
+Tool.Name = "Ping"
+Tool.Activated:Connect(function()
+    local Ping = Instance.new("Sound", workspace)
+    Ping.SoundId = "rbxassetid://7383525713"
+    Ping:Play()
+    Ping.Ended:Connect(function()
+        Ping:Destroy()
+    end)
+end)
